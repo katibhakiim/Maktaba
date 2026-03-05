9df6112a653f5f813d22a189b87ee4a1a207f7e9
katib hakiim <katibhakiim@gmail.com>
2026-03-05 06:39:32 +0100

    Implement GetCategoriesUseCase to retrieve categories
    Implemented the GetCategoriesUseCase to fetch categories from the repository.

--- app/src/main/java/com/ElOuedUniv/maktaba/domain/usecase/GetCategoriesUseCase.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/domain/usecase/GetCategoriesUseCase.kt
@@ -3,11 +3,27 @@ package com.ElOuedUniv.maktaba.domain.usecase
 import com.ElOuedUniv.maktaba.data.model.Category
 import com.ElOuedUniv.maktaba.data.repository.CategoryRepository
 
+/**
+ * Use case to retrieve all available book categories from the library system.
+ * 
+ * Acts as the bridge between the UI/ViewModel and the Data layer, 
+ * ensuring that data access follows the app's business rules.
+ */
+
 // TODO: Implement this use case
 class GetCategoriesUseCase(
     private val categoryRepository: CategoryRepository
 ) {
+       /**
+     * Executes the use case to fetch the categories list.
+     * 
+     * Using the 'operator' keyword allows calling this use case directly 
+     * as a function: getCategoriesUseCase().
+     * 
+     * @return A list of all available [Category] objects.
+     */
+    
     operator fun invoke(): List<Category> {
-        TODO("Not yet implemented")
+     return categoryRepository.getAllCategories()
     }
 }
